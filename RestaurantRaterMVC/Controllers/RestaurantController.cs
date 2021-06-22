425bbbae6e1431d3bd71a39f6a5be037bee0c1ea
Russell Rockwood <russell.rockwood@gmail.com>
2021-06-21 21:25:11 -0500

    added details view

--- RestaurantRaterMVC/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -96,7 +96,20 @@ namespace RestaurantRaterMVC.Controllers
             }
 
             return View(restaurant);
+        }
 
+        // GET: Restaurant/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Restaurant restaurant = _db.Restaurants.Find(id);
+
+            if (restaurant == null)
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+
+            return View(restaurant);
         }
     }
 }
